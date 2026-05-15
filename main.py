358ffd7a65f91d9dfd409f0e44862e43a52681e8
Silvia Pineda Jimenez <157540574+spineda12@users.noreply.github.com>
2026-05-14 22:03:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ Authors: Silvia Pineda Jimenez, Teddy Santoyo, Conrad Fries Reuschling, Christop
 Class: CST205 M/W 2-4 
 Date: May 13 2026
 GitHub Repository: https://github.com/cofries/Cst205-7613-group-project
+Trello link : https://trello.com/invite/b/69e9382f6de4581a66ca8b5c/ATTI763076b1993f31d9729997d9eaa121fe94B4A6B9/team7613
+
+Chris: worked on the meme creation part of the project
+allowing users to upload an image and create/customize their own meme
+
+Teddy: worked on the save and dowload features for users to be able to save their memes directly to their devices.
+
+Conrad: worked on the filters page so users were able to apply filters and crop their memes.
+
+Silvia: worked on setting up the app with the API's to be able to load on the mood tab. Also created the app home page and banner.
+She also customized the layout of the app.
+
 """
 
 
